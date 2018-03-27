a5af3da0ccec04c5ecce79fa34dce1804fc585c9
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2018-03-27 15:10:48 +0100

    Continuing with Chapter 3, exercises

--- src/chapter3/exercises/E3-1.cpp
+++ src/chapter3/exercises/E3-1.cpp
@@ -1,3 +1,20 @@
-int main() {
+#include <iostream>
+#include <iomanip>
 
+using std::setw;
+
+int maiE3_1() {
+	unsigned int num {};
+	std::cout << "Input a positive integer: ";
+	std::cin >> num;
+
+	std::cout << std::hex << std::setfill('0')
+			  << "\nThe values of num and ~num in hexadecimal representation are :   "
+			  << setw(8) << num << "     " << setw(8) << (~num);
+
+	std::cout << std::dec << std::setfill(' ')
+			  << "\nThe values of num and ~num in decimal representation are     :"
+			  << setw(11) << num << "   " << setw(10) << (~num);
+
+	std::cout << std::endl;
 }
