724f95d1cfedbff041f07095859cb56edd39e0df
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2018-03-28 10:05:24 +0100

    Continuing with Chapter 3, exercises

--- src/chapter3/exercises/E3-3.cpp
+++ src/chapter3/exercises/E3-3.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main() {
+int mainE3_3() {
 	unsigned int k {430U};
 	unsigned int j {(k >> 4) & ~(~0 << 3)};
 	std::cout << j << std::endl;
---
+++ src/chapter3/exercises/E3-4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+	int num {0};
+	char ch {};
+	std::cout << "Enter four characters: ";
+
+	std::cin >> ch;
+	num |= static_cast<int>(ch);
+
+	num <<= 8;
+	std::cin >> ch;
+	num |= static_cast<int>(ch);
+
+	num <<= 8;
+	std::cin >> ch;
+	num |= static_cast<int>(ch);
+
+	num <<= 8;
+	std::cin >> ch;
+	num |= static_cast<int>(ch);
+
+	std::cout << "The encoding number is: " << std::hex << std::setfill('0') << std::setw(8) << num;
+	std::cout << "\nThe encoded characters are: ";
+	unsigned int mask {0b11111111};
+	std::cout << static_cast<char>(num & mask) << " ";
+	mask <<= 8;
+	std::cout << static_cast<char>((num & mask) >> 8) << " ";
+	mask <<= 8;
+	std::cout << static_cast<char>((num & mask) >> 16) << " ";
+	mask <<= 8;
+	std::cout << static_cast<char>((num & mask) >> 24);
+	std::cout << std::endl;
+}
